02ababe67870b4080182450020c427b6f284730a
Rashed Abdel-Tawab <rashed@linux.com>
2017-01-08 16:36:02 -0500

    msm8996: graphics: Add 32bit libmm-disp-apis and libmm-qdcm
    Change-Id: I3056af3482b1d10b2850c06ae4ae8ba80b5f5139

--- msm8996/graphics/graphics-vendor.mk
+++ msm8996/graphics/graphics-vendor.mk
@@ -64,6 +64,8 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/binaries/msm8996/graphics/proprietary/vendor/lib/libllvm-glnext.so:system/vendor/lib/libllvm-glnext.so \
     vendor/qcom/binaries/msm8996/graphics/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
     vendor/qcom/binaries/msm8996/graphics/proprietary/vendor/lib/libllvm-qgl.so:system/vendor/lib/libllvm-qgl.so \
+    vendor/qcom/binaries/msm8996/graphics/proprietary/vendor/lib/libmm-disp-apis.so:system/vendor/lib/libmm-disp-apis.so \
+    vendor/qcom/binaries/msm8996/graphics/proprietary/vendor/lib/libmm-qdcm.so:system/vendor/lib/libmm-qdcm.so \
     vendor/qcom/binaries/msm8996/graphics/proprietary/vendor/lib/libmmQSM.so:system/vendor/lib/libmmQSM.so \
     vendor/qcom/binaries/msm8996/graphics/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
     vendor/qcom/binaries/msm8996/graphics/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
